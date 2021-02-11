089bbe09590960190f4cc20a8b1ee1424ed423e5
JazzGlobal <waymer147@gmail.com>
2021-02-11 13:48:15 -0500

    Add project files.

---
+++ EFDatabaseTask/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model; 
+
+namespace EFDatabaseTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private U07lyXEntities dbcontext = new U07lyXEntities(); 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
