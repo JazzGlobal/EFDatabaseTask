b3a2aa2ee50e6e747203adaf45b26de4861f1cce
JazzGlobal <waymer147@gmail.com>
2021-03-09 12:42:45 -0500

    Added Logger.

---
+++ EFDatabaseTask/Log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDatabaseTask.Logger
+{
+    public static class Log
+    {
+        public static bool LogEvent(string filename,string message)
+        {
+            try
+            {
+                string formatString = $"\n======================================\n {DateTime.Now}: {message} \n======================================\n";
+                File.AppendAllText(filename, formatString);
+                return true;
+            } catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
