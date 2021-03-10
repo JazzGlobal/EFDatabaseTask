5a672d322426131e73a48e86b1dc0a8d803abf54
JazzGlobal <waymer147@gmail.com>
2021-03-10 15:26:20 -0500

    Added

---
+++ EFDatabaseTask/Calendar.cs
@@ -0,0 +1,28 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFDatabaseTask
+{
+    public partial class Calendar : Form
+    {
+        public Calendar()
+        {
+            InitializeComponent();
+        }
+        private U07lyXEntities dbcontext = new U07lyXEntities();
+
+        private void Calendar_Load(object sender, EventArgs e)
+        {
+            // 
+            Console.WriteLine("Getting Appointments For User: " + Login.CurrentLoggedInUser.userName);
+        }
+    }
+}
