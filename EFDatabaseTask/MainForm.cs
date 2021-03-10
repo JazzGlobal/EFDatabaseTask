92eb7c914d80d358c6f3bdf0e782499730a7af65
JazzGlobal <waymer147@gmail.com>
2021-03-10 17:31:27 -0500

    MainForm now continuously runs timer to poll for upcoming events / meetings.

--- EFDatabaseTask/MainForm.cs
+++ EFDatabaseTask/MainForm.cs
@@ -5,21 +5,40 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace EFDatabaseTask
 {
     public partial class MainForm : Form
     {
+        System.Windows.Forms.Timer timer;
+
         public MainForm()
         {
             InitializeComponent();
             this.IsMdiContainer = true;
         }
+        private void CheckForEvents()
+        {
+            Console.WriteLine("Checking for calendar events!");
+            // TODO: Query the list of meetings for the logged in user for meetings that will occur within 15 minutes.
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            new Thread(() =>
+            {
+                CheckForEvents();
+            }).Start();
+        }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            timer = new System.Windows.Forms.Timer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = 60000;
+            timer.Start();
             ShowLoginForm();
         }
         public void ShowLoginForm()
