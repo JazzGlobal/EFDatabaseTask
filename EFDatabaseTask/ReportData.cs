a140e09bd6a1b048c5ec2ac5bf3b0d50c0608f6c
JazzGlobal <waymer147@gmail.com>
2021-03-12 09:04:13 -0500

    Added ReportData. Handles gathering the data for the reporting requirements.

---
+++ EFDatabaseTask/ReportData.cs
@@ -0,0 +1,26 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDatabaseTask
+{
+    public class ReportData
+    {
+        private U07lyXEntities dbcontext
+        {
+            get { return new U07lyXEntities(); }
+        }
+        public List<appointment> GetUserAppointments(Model.user user)
+        {
+            return dbcontext.appointments.Where(appointment => appointment.user.userName == user.userName).OrderBy(appointment => appointment.start).ToList(); 
+        }
+
+        public int GetUniqueAppointmentTypes()
+        {
+            return dbcontext.appointments.Select(appointment => appointment.type).Distinct().Count();
+        }
+    }
+}
