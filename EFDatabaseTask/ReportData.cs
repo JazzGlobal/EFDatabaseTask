0ad05e9af0c08f222e4d18205d1680674a302d93
JazzGlobal <waymer147@gmail.com>
2021-03-12 09:19:04 -0500

    Added comments and more selection criteria for GetUserAppointments.

--- EFDatabaseTask/ReportData.cs
+++ EFDatabaseTask/ReportData.cs
@@ -1,4 +1,5 @@
 ﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,11 +11,27 @@ namespace EFDatabaseTask
         {
             get { return new U07lyXEntities(); }
         }
+        /// <summary>
+        /// Queries the appointment table for all appointments from current day onward, order by appointment start.
+        /// </summary>
+        /// <param name="user">User whose appointments are desired.</param>
+        /// <returns></returns>
         public List<appointment> GetUserAppointments(Model.user user)
         {
-            return dbcontext.appointments.Where(appointment => appointment.user.userName == user.userName).OrderBy(appointment => appointment.start).ToList();
+            // Return all appointments from current day onward, ordered by appointment start. This makes iterating in the reports easier.
+            return dbcontext.appointments
+                .Where(appointment => 
+            appointment.user.userName == user.userName 
+            && appointment.start.Month == DateTime.Now.Month
+            && appointment.start.Day >= DateTime.Now.Day)
+                .OrderBy(appointment => appointment.start)
+                .ToList();
         }
 
+        /// <summary>
+        /// Returns integer value denoting the total unique types of appointments in the current database.
+        /// </summary>
+        /// <returns></returns>
         public int GetUniqueAppointmentTypes()
         {
             return dbcontext.appointments.Select(appointment => appointment.type).Distinct().Count();
