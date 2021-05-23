be0c1030da54ef747fc27094ba776caced171af1
JazzGlobal <waymer147@gmail.com>
2021-05-23 15:07:07 -0400

    Added unit tests under ReportDataTests.cs

---
+++ EFDatabaseTaskTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EFDatabaseTaskTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EFDatabaseTaskTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac4f35ed-2771-4c42-9d4e-24df38ca646e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EFDatabaseTaskTests/ReportDataTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using EFDatabaseTask;
+using Model;
+
+namespace EFDatabaseTaskTests
+{
+    [TestClass]
+    public class ReportDataTests
+    {
+        private U07lyXEntities dbcontext
+        {
+            get { return new U07lyXEntities(); }
+        }
+        /// <summary>
+        /// Executes stored procedure "clean_database". Truncates the PET, customer, and appointment tables.
+        /// </summary>
+        private void CleanDatabase()
+        {
+            dbcontext.clean_database();
+        }
+        [TestMethod]
+        public void GetAllActivePets_ResetDBAdd2Pets_Return2()
+        {
+            // Create test data.  
+            dbcontext.unit_test_1(); // Stored Procedure.
+
+            // Query database
+            ReportData r_data = new ReportData();
+            var results = r_data.GetAllActivePets();
+
+            // Assert database results.
+            Assert.AreEqual(results.Count, 2);
+
+            // Cleanup
+            CleanDatabase();
+        }
+        [TestMethod]
+        public void GetAllActiveCustomers_ResetDBAdd2Customers_Return2() 
+        {
+            // Create test data.
+            dbcontext.unit_test_1();
+
+            // Query database
+            ReportData r_data = new ReportData();
+            var results = r_data.GetAllActiveCustomers();
+
+            // Assert database results. 
+            Assert.AreEqual(results.Count, 2);
+
+            // Cleanup
+            CleanDatabase();
+        }
+    }
+}
